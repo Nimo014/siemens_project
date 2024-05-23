c41b46331a396959d565956aa00999b087d9b65e
Nirav Parmar <73212864+Nimo014@users.noreply.github.com>
2024-05-23 12:00:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 import streamlit as st
-import langchain_helper
+from langchain.llms import LlamaCpp
+
+llm_cpp = LlamaCpp(
+            streaming = True,
+            model_path="/kaggle/input/siemens-langchain/zephyr-7b-beta.Q4_0.gguf",
+            n_gpu_layers=2,
+            n_batch=512,
+            temperature=0.96,
+            top_p=1,
+            verbose=True,
+            n_ctx=4096
+            )
+
+def generate_restaurant_name_and_items(cuisine):
+    # Chain 1: Restaurant Name
+    prompt_template_name = PromptTemplate(
+        input_variables=['cuisine'],
+        template="I want to open a restaurant for {cuisine} food. Suggest a fancy name for this."
+    )
+
+    name_chain = LLMChain(llm=llm_cpp, prompt=prompt_template_name, output_key="restaurant_name")
+
+    # Chain 2: Menu Items
+    prompt_template_items = PromptTemplate(
+        input_variables=['restaurant_name'],
+        template="""Suggest some menu items for {restaurant_name}. Return it as a comma separated string"""
+    )
+
+    food_items_chain = LLMChain(llm=llm_cpp, prompt=prompt_template_items, output_key="menu_items")
+
+    chain = SequentialChain(
+        chains=[name_chain, food_items_chain],
+        input_variables=['cuisine'],
+        output_variables=['restaurant_name', "menu_items"]
+    )
+
+    response = chain({'cuisine': cuisine})
+
+    return response
 
 st.title("Restaurant Name Generator")
 
 cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian", "Mexican", "Arabic", "American"))
 
 if cuisine:
-    response = langchain_helper.generate_restaurant_name_and_items(cuisine)
+    response = generate_restaurant_name_and_items(cuisine)
     st.header(response['restaurant_name'].strip())
     menu_items = response['menu_items'].strip().split(",")
     st.write("**Menu Items**")
